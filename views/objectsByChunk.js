981cb687cae567ef160355fa24f1b7616b63e8a3
Brandon Wiley <brandon@blanu.net>
2011-01-03 17:36:52 -0600

    Added views

---
+++ views/objectsByChunk.js
@@ -0,0 +1,45 @@
+var Request = require("ringo/webapp/request").Request
+var logging = require("google/appengine/logging")
+
+exports.app = function (request) {
+    return exports[request.method](request);
+}
+
+exports.GET = function (request) {
+    logging.info("view get...");
+    var params = new Request(request).params
+    logging.info("params: "+JSON.stringify(params));
+
+    return {
+        status: 200,
+        headers: {"Content-Type": "application/json"},
+        body: ["null"]
+    };
+}
+
+exports.POST = function (request) {
+    logging.info("view post...");
+    var params = new Request(request).params
+    logging.info("params: "+JSON.stringify(params));
+
+    if(params['type']=='object')
+    {
+      var chunkX=params['chunkX'];
+      var chunkY=params['chunkY'];
+      var chunkId=chunkX+'_'+chunkY;
+
+      return {
+        status: 200,
+        headers: {"Content-Type": "application/json"},
+        body: ["{\"key\": \""+chunkId+"\", \"value\": "+JSON.stringify(params)+"}"]
+      };
+    }
+    else
+    {
+      return {
+        status: 200,
+        headers: {"Content-Type": "application/json"},
+        body: ["null"]
+      };
+    }
+}
