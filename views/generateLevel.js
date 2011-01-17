1dd07ae87d56663d9226cc257efcd3eb5728c317
Brandon Wiley <brandon@blanu.net>
2011-01-16 22:17:34 -0600

    Stub for level generator error handler

---
+++ views/generateLevel.js
@@ -0,0 +1,37 @@
+var Request = require("ringo/webapp/request").Request
+var logging = require("google/appengine/logging")
+
+exports.app = function (request) {
+    return exports[request.method](request);
+}
+
+exports.GET = function (request) {
+    logging.info("view get...");
+    var params = new Request(request).params
+    logging.info("params: "+JSON.stringify(params));
+
+    return {
+        status: 200,
+        headers: {"Content-Type": "application/json"},
+        body: ["null"]
+    };
+}
+
+exports.POST = function (request) {
+    logging.info("generating Level...");
+    var params = new Request(request).params
+    logging.info("params: "+JSON.stringify(params));
+
+    params=JSON.parse(params['value']);
+    logging.info("params: "+JSON.stringify(params));
+
+    var levelName=params;
+
+    logging.info('Generating '+levelName);
+
+    return {
+      status: 200,
+      headers: {"Content-Type": "application/json"},
+      body: ["null"]
+    };
+}
