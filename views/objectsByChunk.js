9b46544b7ab2a084d03c7c2a052ddf7b586fceb9
Brandon Wiley <brandon@blanu.net>
2011-05-04 21:44:20 -0500

    Changed to new nodejs-based format

--- views/objectsByChunk.js
+++ views/objectsByChunk.js
@@ -1,51 +1,15 @@
-var Request = require("ringo/webapp/request").Request
-var logging = require("google/appengine/logging")
-
-exports.app = function (request) {
-    return exports[request.method](request);
-}
-
-exports.GET = function (request) {
-    logging.info("view get...");
-    var params = new Request(request).params
-    logging.info("params: "+JSON.stringify(params));
-
-    return {
-        status: 200,
-        headers: {"Content-Type": "application/json"},
-        body: ["null"]
-    };
-}
-
-exports.POST = function (request) {
-    logging.info("view post...");
-    var params = new Request(request).params
-    logging.info("params: "+JSON.stringify(params));
-
-    params=JSON.parse(params['value']);
-    logging.info("params: "+JSON.stringify(params));
-
+exports.map = function (params)
+{
     if(params['type']=='object')
     {
       var chunkX=params['chunkX'];
       var chunkY=params['chunkY'];
       var chunkId=chunkX+'_'+chunkY;
 
-      logging.info("object");
-
-      return {
-        status: 200,
-        headers: {"Content-Type": "application/json"},
-        body: ["{\"key\": \""+chunkId+"\", \"value\": "+JSON.stringify(params)+"}"]
-      };
+      return ["{\"key\": \""+chunkId+"\", \"value\": "+JSON.stringify(params)+"}"];
     }
     else
     {
-      logging.info("not object");
-      return {
-        status: 200,
-        headers: {"Content-Type": "application/json"},
-        body: ["null"]
-      };
+      return ["null"];
     }
 }
